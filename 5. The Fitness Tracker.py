6f3fd95206165bb692cd76cb216bdc22de69bddd
jlake70 <jlake7021@gmail.com>
2024-06-12 14:08:52 -0400

    Finished Question 5

--- 5. The Fitness Tracker.py
+++ 5. The Fitness Tracker.py
@@ -3,15 +3,37 @@
 def fitness_tracker():
 
     activities = ["Dancing", "Biking", "Swimming", "Running"]
-    duration = [10, 15, 20, 30]
+    durations = [10, 15, 20, 30]
 
-    activity_input = input("What activity are you going to perform today?: ")
-
-    if activity_input in activities:
-        index = activities.index(activity_input)
-        print(f"The duration of this workout is: {duration[index]} minutes.")
+    activity_input = input("What activity are you going to perform today?: ").lower()
+    activities_lower = [activity.lower() for activity in activities]
+    
+    if activity_input in activities_lower:
+        index = activities_lower.index(activity_input)
+        print(f"The duration of this workout is: {durations[index]} minutes.")
     else:
         print("The activity is not on the list.")
        
 
 fitness_tracker()
+
+# Task 2 and Task 3
+
+def calories_burned():
+    activities = ["Dancing", "Biking", "Swimming", "Running"]
+    durations = [10, 15, 20, 30]
+    
+
+    activity_input = input("What activity are you going to perform today so we can calculate the calories burned?: ").lower()
+    activities_lower = [activity.lower() for activity in activities]
+    
+    if activity_input in activities_lower:
+        index = activities_lower.index(activity_input)
+        duration = durations[index]
+        calories_calc = duration * 3.5
+
+        print(f"The activity you chose was: {activities[index]} \n The duration of this workout is: {durations[index]} minutes. \n You burned {calories_calc} calories.")
+    else:
+        print("The activity is not on the list.")
+
+calories_burned()
